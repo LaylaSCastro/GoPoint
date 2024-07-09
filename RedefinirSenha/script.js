500a450b4bfcdbdde91e922ad4ee603dfc2e98fc
LaylaSCastro <laylacristinasilvacastro@gmail.com>
2024-07-09 19:50:35 -0300

    Reset screen finished

--- RedefinirSenha/script.js
+++ RedefinirSenha/script.js
@@ -12,4 +12,14 @@ document.getElementById('togglePassword').addEventListener('click', function (e)
     password.setAttribute('type', type);
 
     this.classList = type === 'password' ? 'ph ph-eye-closed' : 'ph ph-eye';
+    console.log(this)
+});
+
+document.getElementById('toggleConfirmPassword').addEventListener('click', function (e) {
+
+    const password = document.querySelector('.confirm-password-input');
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+
+    this.classList = type === 'password' ? 'ph ph-eye-closed' : 'ph ph-eye';
 });
