c02ecf91690910b4dcd2d6629bcf3f30222d2197
LaylaSCastro <laylacristinasilvacastro@gmail.com>
2024-07-15 18:10:07 -0300

    editing signUp

--- Cadastro/script.js
+++ Cadastro/script.js
@@ -19,3 +19,31 @@ btn.addEventListener('click',()=>{
         console.log("Resultado: ",response)
     })
 })
+
+const form = document.querySelector('.password-container')
+
+console.log(form)
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+
+}
+
+document.getElementById('togglePassword').addEventListener('click', function (e) {
+
+    const password = document.querySelector('.password-input');
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+
+    this.classList = type === 'password' ? 'ph ph-eye-closed' : 'ph ph-eye';
+    console.log(this)
+});
+
+document.getElementById('toggleConfirmPassword').addEventListener('click', function (e) {
+
+    const password = document.querySelector('.confirm-password-input');
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+
+    this.classList = type === 'password' ? 'ph ph-eye-closed' : 'ph ph-eye';
+});
